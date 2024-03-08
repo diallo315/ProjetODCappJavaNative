937343389318a0885efb8966574d1c6f332cd5a5
diallo315 <diallomika866@gmail.com>
2024-03-08 12:29:11 +0000

    two commit

--- app/src/main/java/com/hnf/odc1/pageprincipale.java
+++ app/src/main/java/com/hnf/odc1/pageprincipale.java
@@ -14,6 +14,9 @@ public class pageprincipale extends AppCompatActivity {
 
     Button button;
     TextView txtcontent, txterror;
+    boolean buttoncliked;
+
+
     @Override
     protected void onCreate(@Nullable Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
@@ -22,14 +25,39 @@ public class pageprincipale extends AppCompatActivity {
         txtcontent = findViewById(R.id.textframeContent);
         txterror = findViewById(R.id.textframeError);
 
+        if(saveInstanceState !=null){
+            txtcontent.setText(saveInstanceState.getString("cleTexte"));
+            txtcontent.setVisibility(saveInstanceState.getInt("txtcontentvisibility", View.VISIBLE));
+
+            txterror.setText(saveInstanceState.getString("cleTexte1"));
+            txterror.setVisibility(saveInstanceState.getInt("txtcontentvisibility1", View.VISIBLE));
+
+            buttoncliked = saveInstanceState.getBoolean("buttonholed", false);
+            if(buttoncliked){
+                button.setBackgroundColor(Color.RED);
+            }
+        }
+
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 txtcontent.setVisibility(View.GONE);
                 txterror.setVisibility(View.VISIBLE);
                 button.setBackgroundColor(Color.RED);
+                buttoncliked = true;
             }
         });
 
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outSate) {
+        super.onSaveInstanceState(outSate);
+        outSate.putString("cleTexte", txtcontent.getText().toString());
+        outSate.putString("cleTexte1", txterror.getText().toString());
 
-    }}
+        outSate.putBoolean("buttonholed", buttoncliked);
+
+        outSate.putInt("txtcontentvisibility", txtcontent.getVisibility());
+        outSate.putInt("txtcontentvisibility1", txterror.getVisibility());
+    }
+}
