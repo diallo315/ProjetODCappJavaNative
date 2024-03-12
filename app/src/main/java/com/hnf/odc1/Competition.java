ae1eb619e4b72cede5765d9b2d86210c0c54e3d2
= <diallomika866@gmail.com>
2024-03-12 16:58:45 +0000

    three commit

---
+++ app/src/main/java/com/hnf/odc1/Competition.java
@@ -0,0 +1,14 @@
+package com.hnf.odc1;
+
+public class Competition {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ app/src/main/java/com/hnf/odc1/CompetitionsReponse.java
@@ -0,0 +1,16 @@
+package com.hnf.odc1;
+
+import java.util.List;
+
+public class CompetitionsReponse {
+    private int count;
+    private List<Competition> competitions;
+
+    public int getCount() {
+        return count;
+    }
+
+    public List<Competition> getCompetitions() {
+        return competitions;
+    }
+}
